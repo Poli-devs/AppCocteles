69f0fc5fa5b1df007fc92f3d2877662483ceea70
Poli-devs <luisfercuji@gmail.com>
2025-11-19 17:26:38 -0500

    Update in server archive conexion.js

--- server/src/database/conexion.js
+++ server/src/database/conexion.js
@@ -1,18 +1,19 @@
-import { Sequelize } from "sequelize";
+// conexion.js
+import Sequelize from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const DIALECT = process.env.DB_DIALECT || "postgres";
-export const sequelize = new Sequelize(
-    process.env.DB_NAME || "AppCocktail",
-    process.env.DB_USER || "postgres",
-    process.env.DB_PASSWORD || "123456",
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
-        host: process.env.DB_HOST || "localhost",
-        dialect: DIALECT,
-        timezone: process.env.DB_TIMEZONE || "+00:00",
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        timezone: process.env.DB_TIMEZONE,
         logging: process.env.DB_LOGGING === "true" ? console.log : false,
     }
 );
 
 export const regenerarBD = false;
+export { sequelize };
